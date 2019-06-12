7b85783358a6dede2be29db0aec63c979ada27f2
AnikMallick <apmallick0507@gmail.com>
2019-06-12 15:16:25 +0530

    added title

--- plot1.R
+++ plot1.R
@@ -6,6 +6,7 @@ png("plot1.png",width = 480,height = 480)
 
 #plot 1
 hist(df$Global_active_power,main = "Global Active Power",
-     xlab = "Global Active Power (kilowatts)",col = "red")
+     xlab = "Global Active Power (kilowatts)",col = "red",
+     main = "Global Active Power")
 
 dev.off()
